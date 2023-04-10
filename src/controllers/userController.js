f6d3b7d5244c1ec0b9d7dde4b9ce1332995fef4d
Vu Duc <vuduc1611@gmail.com>
2023-04-10 11:29:13 +0700

    Nodejs:  Hien thi du lieu ra View

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,7 +25,7 @@ let handleLogin = async (req, res) => {
 }
 
 let handleGetAllUsers = async (req, res) => {
-    let id = req.body.id;
+    let id = req.query.id;// All, id
 
     if (!id) {
         return res.status(200).json({
